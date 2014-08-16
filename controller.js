dfa1bbbde912c8e322bd54e09a2edec8881d99e7
Blair <blair@hpmini210.(none)>
2014-08-16 13:37:03 +1000

    controllerFileupload.js

---
+++ controller.js
@@ -0,0 +1,248 @@
+﻿function init() {
+	$('#divModalItemDetail').modal({ show: false});
+	$('#divModalUpdateDetail').modal({ show: false});
+	$('#divModalUpload').modal({ show: false});
+	
+	document.getElementById("inputNewTitle").value = "";
+	document.getElementById("inputNewDescription").value = "";
+	document.getElementById("inputDocumentTitle").value = "";
+	drawTable();
+	document.getElementById("buttonUpload").disabled = false;
+	document.getElementById("buttonSave").disabled = false;
+	
+	$('#buttonUpload').tooltip({title:"Open/import a JSON file in your computer's filesystem", container: 'body', placement: 'bottom'}); 
+	$('#buttonSave').tooltip({title:"Save/export as JSON file", container: 'body', placement: 'bottom'}); 
+}
+
+function drawTable() {
+	var tableIssues = document.getElementById("tableIssues");
+	
+	// clear existing
+	while (tableIssues.hasChildNodes()) {
+		tableIssues.removeChild(tableIssues.lastChild);
+	}
+	
+	// reprint header
+	var trHeader = document.createElement("tr");
+	var thHeader = document.createElement("th");
+	thHeader.appendChild(document.createTextNode(""));
+	trHeader.appendChild(thHeader);
+	
+	// print all unique dates
+	uniqueDaysFromData();
+	for (i = 0; i < singletonUniqueDates.length; i++) {
+		var thHeaderDate = document.createElement("th");
+		thHeaderDate.className = "thHeaderDate";
+		if (
+			compareTwoDates(singletonUniqueDates[i], new Date()) == 0
+		) {
+			thHeaderDate.className = "thHeaderDateToday";
+		}
+		thHeaderDate.appendChild(document.createTextNode(
+			moment(singletonUniqueDates[i]).format("Do MMM ’YY")
+		));
+		trHeader.appendChild(thHeaderDate);
+	}
+	
+	tableIssues.appendChild(trHeader);
+	
+	// add all singletonData
+	for (i = 0; i < singletonData.length; i++) {
+		var trData = document.createElement("tr");
+		var tdData = document.createElement("td");
+		tdData.className = 'tdIssues';
+		tdData.innerHTML = '<a href="#" onclick="showModalItemDetail(\''+ singletonData[i].uuid + '\', null)">' + singletonData[i].title + " <strong>(" + singletonData[i].updates.length + ")</strong>" + '</a>';
+		trData.appendChild(tdData);
+		
+		if (singletonData[i].updates.length > 0) {
+			// there are updates
+			
+			for (j = 0; j < singletonUniqueDates.length; j++) {
+				// go through each unique date
+				
+				var tdDataDate = document.createElement("td");
+				for (k = 0; k < singletonData[i].updates.length; k++) {
+				
+					// go through each update
+					if (compareTwoDates(singletonUniqueDates[j], singletonData[i].updates[k].timestamp) == 0) {
+						var proposedValue = singletonData[i].updates[k].note;
+						var existingClass = tdDataDate.className;
+						var toBeUsedValue = null;
+						
+						if (existingClass == 'issueClose') {
+							// if it's closed then we can't override it
+						} else if (existingClass == 'issueOpen' && proposedValue == 'x') {
+							// open is only overridden by close, not by continue
+							toBeUsedValue = 'x';
+						} else if (existingClass == 'issueOpen' && proposedValue != 'x') {
+							// do nothing
+						} else {
+							// could be x, o, or >
+							toBeUsedValue = proposedValue;
+						}
+						
+						if (toBeUsedValue == 'o') {
+							tdDataDate.className = "issueOpen";
+						} else if (toBeUsedValue == '>') {
+							tdDataDate.className = "issueContinue";
+						} else if (toBeUsedValue == 'x') {
+							tdDataDate.className = "issueClose";
+						}
+						
+					} else {
+						if (tdDataDate.innerHTML == "") {
+							toBeUsedValue = ".";
+							tdDataDate.className = "issueNotHere";
+						}
+					}
+					
+					if (toBeUsedValue) {
+						tdDataDate.innerHTML = '<a href="#" onclick="showModalItemDetail(\''+ singletonData[i].uuid + '\', \'' + getJustTheDate(singletonUniqueDates[j]) + '\')">' + toBeUsedValue + '</a>';
+					}
+				}
+				trData.appendChild(tdDataDate);
+			}
+		}
+		
+		tableIssues.appendChild(trData);
+	}
+	
+}
+
+function addNew() {
+	if (document.getElementById("inputNewTitle").value != "") {
+		createItem(
+			generateUUID(),
+			document.getElementById("inputNewTitle").value,
+			document.getElementById("inputNewDescription").value,
+			[ { timestamp: new Date(), note: "o" } ]
+		);
+		drawTable();
+		document.getElementById("inputNewTitle").value = "";
+		document.getElementById("inputNewDescription").value = "";
+	} else {
+		alert("You need to have a title.");
+	}
+}
+
+function showModalItemDetail(uuid, datestring) {
+	singletonSelectedUUID = uuid;
+	singletonSelectedDate = datestring;
+	
+	var thisObject = readItemByUUID(uuid);
+	document.getElementById("divModalItemDetailLabel").innerHTML = thisObject.title + (datestring ? ", <strong>" + moment(datestring).format("Do MMM ’YY") + "</strong>" : "");
+	document.getElementById("modalUUID").innerHTML = uuid;
+	document.getElementById("modalTitle").value = thisObject.title;
+	document.getElementById("modalDescription").value = thisObject.description;
+	
+	var updatesText = "<tr><th>Timestamp</th><th>Note</th><th>Description</th></tr>"
+		+ '<tr><td colspan="3"><button onclick="showModalUpdateDetail(\''+uuid+'\')">Add update</button></td></tr>';
+		
+	for (i = thisObject.updates.length - 1; i >= 0 ; i--) {
+	// we want to print the objects backwards so the most recent is on top
+		if (
+			datestring == null // no particular day selected
+			|| (datestring != null && compareTwoDates(thisObject.updates[i].timestamp, datestring) == 0) // particular day selected
+		) {
+			if (thisObject.updates[i].note == 'o') {
+				thisClass = "danger";
+			} else if (thisObject.updates[i].note == ">") {
+				thisClass = "warning";
+			} else if (thisObject.updates[i].note == "x") {
+				thisClass = "success";
+			}
+			updatesText = updatesText
+				+ '<tr><td style="white-space: nowrap;">'
+				+ '<a href="#" onclick="showModalUpdateDetail()">' + moment(thisObject.updates[i].timestamp).format() + '</a>'
+				+ '</td><td class="'+ thisClass +'" style="text-align: center; font-weight: bold;">'
+				+ thisObject.updates[i].note
+				+ '</td><td><div class="forceWrap">'
+				+ (thisObject.updates[i].description ? thisObject.updates[i].description : "<em>No description</em>")
+				+ "</div></td></tr>";
+		}
+	}
+	document.getElementById("tableUpdates").innerHTML = updatesText;
+
+	$('#divModalItemDetail').modal('show');
+}
+
+function showModalUpdateDetail() {
+	$('#divModalItemDetail').modal('hide');
+	$('#divModalUpdateDetail').modal('show');
+		
+	// clear for next use
+	document.getElementById("mudDate").value = moment().format();
+	document.getElementById("mudDescription").value = "";
+	document.getElementById("mudDescription").focus();
+}
+
+function hideModalUpdateDetail() {
+	$('#divModalUpdateDetail').modal('hide');
+	showModalItemDetail(singletonSelectedUUID, singletonSelectedDate);
+}
+
+function showModalUpload() {
+	$('#divModalUpload').modal('show');
+}
+
+
+function download() {
+	var blob = new Blob([JSON.stringify(singletonData, null, '\t').replace(/\r?\n/g, '\r\n')], {type: "text/plain;charset=utf-8"});
+	if (document.getElementById("inputDocumentTitle").value) {
+		var filename = document.getElementById("inputDocumentTitle").value + ".json";
+	} else {
+		var filename = "untitled_issue_tracker.json";
+	}
+	saveAs(blob, filename);
+}
+
+function saveChanges() {
+	var uuid = document.getElementById("modalUUID").innerHTML;
+	var title = document.getElementById("modalTitle").value;
+	var description = document.getElementById("modalDescription").value;
+	
+	updateItemByUUID(uuid, title, description);
+	$('#divModalItemDetail').modal('hide');
+	drawTable();
+}
+
+function pullDocumentTitleFromInput() {
+	var documentInput = document.getElementById("inputDocumentTitle").value;
+	documentInput = generateValidFilename(documentInput);
+	
+	// update into singleton variable and also document title
+	document.getElementById("inputDocumentTitle").value = documentInput;
+	document.title = documentInput + ".json";
+}
+
+function saveModalUpdateDetail() {
+	console.log(singletonSelectedUUID);
+	var mudDate = document.getElementById("mudDate").value;
+	var mudDateObject = new Date(mudDate);
+	console.log(mudDate);
+	console.log(mudDateObject);
+	
+	var thisNote = "";
+	if (mudTypeOpened.checked) {
+		thisNote = "o";
+	} else if (mudTypeContinue.checked) {
+		thisNote = ">";
+	} else if (mudTypeClosed.checked) {
+		thisNote = "x";
+	}
+	
+	var update = {
+		timestamp: new Date(mudDate),
+		note: thisNote,
+		description: document.getElementById("mudDescription").value
+	}
+	
+	if (update.note) {
+		var index = readItemIndexInArrayByUUID(singletonSelectedUUID);
+		singletonData[index].updates.push(update);
+		drawTable();
+		hideModalUpdateDetail();
+	} else {
+		alert("Aborted");
+	}
+}
---
+++ model.js
@@ -0,0 +1,221 @@
+﻿var singletonData = [];
+var singletonUniqueDates = [];
+
+var singletonSelectedUUID = "";
+var singletonSelectedDate = "";
+var singletonSelectedUpdate = "";
+
+var MONTHS = [
+	"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+];
+
+/*
+ * CRUD functions
+ */
+function createItem(myUUID, myTitle, myDescription, myUpdates) {
+	var newObject = {
+		uuid: myUUID,
+		title: myTitle,
+		description: myDescription,
+		updates: myUpdates
+	}
+	singletonData.push(newObject);
+}
+function readItemByUUID(selectedUUID) {
+	var returnItem = -1;
+	for (i = 0; i < singletonData.length; i++) {
+		if (singletonData[i].uuid == selectedUUID) {
+			returnItem = singletonData[i];
+		}
+	}
+	
+	return returnItem;
+}
+function readItemIndexInArrayByUUID(uuid) {
+	var index = -1;
+	for (i = 0; i < singletonData.length; i++) {
+		if (singletonData[i].uuid == uuid) {
+			index = i;
+		}
+	}
+	
+	return index;
+}
+function updateItemByUUID(myUUID, myTitle, myDescription) {
+	var index = -1;
+	for (i = 0; i < singletonData.length; i++) {
+		if (singletonData[i].uuid == myUUID) {
+			index = i;
+		}
+	}
+	
+	if (index != -1) {
+		singletonData[index].title = myTitle;
+		singletonData[index].description = myDescription;
+	}
+}
+
+
+function compareDateObjects(date1, date2) {
+	returnValue = 0;
+	if (date1.getMilliseconds() < date2.getMilliseconds()) {
+		returnValue = -1;
+	} else if (date1.getMilliseocnds() > date2.getMilliseconds()) {
+		returnValue = 1;
+	}
+	return returnValue;
+}
+
+function compareItemsByTitle(item1, item2) {
+	returnValue = 0;
+	if (item1.title < item2.title) {
+		returnValue = -1;
+	} else if (item1.title > item2.title) {
+		returnValue = 1;
+	}
+	return returnValue;
+}
+
+function compareItemsByAge(item1, item2) {
+	returnValue = 0;
+	
+	varItem1oldestTime = item1.updates[0].timestamp;
+	varItem2oldestTime = item2.updates[0].timestamp;
+	for (i = 0; i < item1.updates.length; i++) {
+		if (compareTwoDates(item1.updates[i].timestamp, varItem1oldestTime) == -1) {
+			varItem1oldestTime = item1.updates[i].timestamp;
+		}
+	}
+	for (i = 0; i < item2.updates.length; i++) {
+		if (compareTwoDates(item2.updates[i].timestamp, varItem2oldestTime) == -1) {
+			varItem2oldestTime = item2.updates[i].timestamp;
+		}
+	}
+	
+	returnValue = compareTwoDates(varItem1oldestTime, varItem2oldestTime);
+	
+	return returnValue;
+}
+
+function compareItemsByAgeReversed(item1, item2) {
+	return compareItemsByAge(item1, item2) * -1;
+}
+
+function compareItemsByLastUpdate(item1, item2) {
+	returnValue = 0;
+	
+	varItem1newestTime = item1.updates[0].timestamp;
+	varItem2newestTime = item2.updates[0].timestamp;
+	for (i = 0; i < item1.updates.length; i++) {
+		if (
+			compareTwoDates(item1.updates[i].timestamp, varItem1newestTime) == 1
+		) {
+			varItem1newestTime = item1.updates[i].timestamp;
+		}
+	}
+	for (i = 0; i < item2.updates.length; i++) {
+		if (
+			compareTwoDates(item2.updates[i].timestamp, varItem2newestTime) == 1
+		) {
+			varItem2newestTime = item2.updates[i].timestamp;
+		}
+	}
+	
+	returnValue = compareTwoDates(varItem1newestTime, varItem2newestTime);
+	
+	return returnValue;
+}
+
+function compareItemsByLastUpdateReversed(item1, item2) {
+	return compareItemsByLastUpdate(item1, item2) * -1;
+}
+
+// http://stackoverflow.com/questions/2698725/comparing-date-part-only-without-comparing-time-in-javascript
+function compareTwoDates(date1in, date2in) {
+	var date1 = getJustTheDate(date1in);
+	var date2 = getJustTheDate(date2in);
+	if (
+		date1.getTime() < date2.getTime()
+	) {
+		return -1;
+	} else if (
+		date1.getTime() > date2.getTime()
+	) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+function getJustTheDate(datetimeObject) {
+	var outputObject = new Date(datetimeObject);
+	outputObject.setHours(0,0,0,0);
+	return outputObject;
+}
+
+function uniqueDaysFromData() {
+	for (i = 0; i < singletonData.length; i++) {
+		for (j = 0; j < singletonData[i].updates.length; j++) {
+			var thisTimestamp = singletonData[i].updates[j].timestamp;
+			var alreadyExists = false;
+			
+			for (k = 0; k < singletonUniqueDates.length; k++) {
+				// look for matches
+				if (
+					compareTwoDates(singletonUniqueDates[k], thisTimestamp) == 0
+				) {
+					alreadyExists = true;
+				}
+			}
+			
+			if (!alreadyExists) {
+				// no match found
+				singletonUniqueDates.push(thisTimestamp);
+			}
+		}
+	}
+	singletonUniqueDates.sort(compareTwoDates);
+}
+
+// http://stackoverflow.com/questions/10073699/pad-a-number-with-leading-zeros-in-javascript
+function pad(n, width, z) {
+	z = z || '0';
+	n = n + '';
+	return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
+// http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
+function generateUUID(){
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x7|0x8)).toString(16);
+    });
+    return uuid;
+};
+
+function niceDateString(d) {
+	var tz = d.getTimezoneOffset()/(-60)
+	return  d.getFullYear().toString().substring(2,4) + "." + pad(d.getMonth(),2) + "." + pad(d.getDate(),2)
+		+ " " + pad(d.getHours(),2) + ":" + pad(d.getMinutes(),2) + ":" + pad(d.getSeconds(),2)
+		+ " utc" + (tz < 0 ? "-"+tz : "+"+tz);
+}
+
+function generateValidFilename(stringInput) {
+	// replace nasty characters
+	var replaceChar = "_";
+	var regEx = new RegExp('[,/\:*$?""<> |]', 'g');
+	var filename = stringInput.replace(regEx, replaceChar);
+	filename = filename.replace(/'/g, replaceChar);
+	
+	// remove opening dots
+	while (filename[0] == ".") {
+		filename = filename.substring(1);
+	}
+	
+	// lowercase
+	filename = filename.toLowerCase();
+	
+	return filename;
+}
